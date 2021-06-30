fba26f65c4aaafc8ec9367976db3a0171f7d7615
Raghadsaud97 <86537522+Raghadsaud97@users.noreply.github.com>
2021-06-30 05:50:26 +0300

    Add files via upload

---
+++ task2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel ="stylesheet" type="text/css" href="task22.css">
+
+</head>
+<body>
+
+<h1> RoboticBase control panel</h1>
+ <pre>
+<button type="button" id="R" >STOP</button>
+<br><br>
+<br>
+<button type="button" id="R1"  >RIGHT &#8594 </button>
+<br>
+<br>
+<br>
+<button type="button" id="R2" >LEFT &#8592</button>
+<br>
+<br>
+<br>
+<button type="button" id="R3" >FORWARD &#8593 </button>
+<br>
+<br>
+<br>
+<button type="button" id="R4" >BACKWARD &#8595</button>
+  </pre> 
+
+
+
+</body>
+</html>
---
+++ task2.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2021 at 04:49 AM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `task2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `base`
+--
+
+CREATE TABLE `base` (
+  `stop` varchar(10) NOT NULL,
+  `right` varchar(10) NOT NULL,
+  `left` varchar(10) NOT NULL,
+  `forward` varchar(10) NOT NULL,
+  `backward` varchar(10) NOT NULL,
+  `id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `base`
+--
+ALTER TABLE `base`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
