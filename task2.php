a3ba4ee55636f259209cd8e627b5e78d0713abba
Raghadsaud97 <86537522+Raghadsaud97@users.noreply.github.com>
2021-07-27 06:04:52 +0300

    Update task2.php

--- task2.php
+++ task2.php
@@ -26,7 +26,20 @@
 <br>
 <button type="button" id="R4" >BACKWARD &#8595</button>
   </pre> 
-
+ 
+<script>
+  window.watsonAssistantChatOptions = {
+      integrationID: "c9dbc90f-f390-412b-b15b-65ac7b4510a5", // The ID of this integration.
+      region: "eu-gb", // The region your integration is hosted in.
+      serviceInstanceID: "d7e74f43-9f88-4184-923e-906e3602ca62", // The ID of your service instance.
+      onLoad: function(instance) { instance.render(); }
+    };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+    document.head.appendChild(t);
+  });
+</script>
 
 
 </body>
